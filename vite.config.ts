86f066737d4ce4e30be0342249662bc5021af0f7
banahaker <pn0818x@outlook.com>
2022-11-05 22:49:57 +0800

    fix: build path

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
-import { defineConfig } from "vite";
-import { fileURLToPath, URL } from "url";
-import vue from "@vitejs/plugin-vue";
+import { defineConfig } from 'vite';
+import { fileURLToPath, URL } from 'url';
+import vue from '@vitejs/plugin-vue';
+import { ghPages } from 'vite-plugin-gh-pages';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  resolve: { alias: { "@": fileURLToPath(new URL("./src", import.meta.url)) } },
+  base: './',
+  plugins: [vue(), ghPages()],
+  resolve: { alias: { '@': fileURLToPath(new URL('./src', import.meta.url)) } },
 });
